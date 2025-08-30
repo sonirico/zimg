5443b680e6aacb2c6f6a86bd4557793fb3c0397e
Marcos <marsanben92@gmail.com>
2025-08-30 18:03:27 +0200

    fix: dereferenced write streamer

--- src/logger.zig
+++ src/logger.zig
@@ -8,9 +8,11 @@ const LogLevel = enum(u8) {
 };
 
 var log_level: LogLevel = .info;
+var writer: *std.io.Writer = undefined;
 
-pub fn init(level: LogLevel) void {
+pub fn init(level: LogLevel, w: *std.io.Writer) void {
     log_level = level;
+    writer = w;
 }
 
 pub fn deinit() void {}
@@ -18,39 +20,33 @@ pub fn deinit() void {}
 /// Print error to stderr and flush immediately
 pub fn err(comptime fmt: []const u8, args: anytype) void {
     if (@intFromEnum(log_level) <= @intFromEnum(LogLevel.err)) {
-        const stderr = std.fs.File.stderr();
-        var stderr_writer = stderr.writerStreaming(&.{}).interface;
-        stderr_writer.print("Error: " ++ fmt ++ "\n", args) catch return;
-        stderr_writer.flush() catch return;
+        writer.print("Error: " ++ fmt ++ "\n", args) catch return;
     }
 }
 
 /// Print warning to stderr and flush immediately
 pub fn warn(comptime fmt: []const u8, args: anytype) void {
     if (@intFromEnum(log_level) <= @intFromEnum(LogLevel.warn)) {
-        const stderr = std.fs.File.stderr();
-        var stderr_writer = stderr.writerStreaming(&.{}).interface;
-        stderr_writer.print("Warning: " ++ fmt ++ "\n", args) catch return;
-        stderr_writer.flush() catch return;
+        writer.print("Warning: " ++ fmt ++ "\n", args) catch return;
     }
 }
 
 /// Print info to stdout and flush immediately
 pub fn info(comptime fmt: []const u8, args: anytype) void {
     if (@intFromEnum(log_level) <= @intFromEnum(LogLevel.info)) {
-        const stdout = std.fs.File.stdout();
-        var stdout_writer = stdout.writerStreaming(&.{}).interface;
-        stdout_writer.print(fmt ++ "\n", args) catch return;
-        stdout_writer.flush() catch return;
+        writer.print(fmt ++ "\n", args) catch return;
     }
 }
 
 /// Print debug to stderr and flush immediately
 pub fn debug(comptime fmt: []const u8, args: anytype) void {
     if (@intFromEnum(log_level) <= @intFromEnum(LogLevel.debug)) {
-        const stderr = std.fs.File.stderr();
-        var stderr_writer = stderr.writerStreaming(&.{}).interface;
-        stderr_writer.print("Debug: " ++ fmt ++ "\n", args) catch return;
-        stderr_writer.flush() catch return;
+        writer.print("Debug: " ++ fmt ++ "\n", args) catch return;
     }
+}
+
+pub fn json(value: anytype, w: ?*std.io.Writer) void {
+    std.json.Stringify.value(value, .{
+        .whitespace = .indent_2,
+    }, w orelse writer) catch return;
 }
--- src/main.zig
+++ src/main.zig
@@ -2,22 +2,29 @@
 const std = @import("std");
 const fs = std.fs;
 const cli = @import("cli.zig");
-
+const logger = @import("logger.zig");
 // Forzar que se incluyan todos los tests
 comptime {
     _ = @import("utils.zig");
 }
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
+    const allocator = std.heap.smp_allocator;
 
-    const file = fs.File.stdout();
-    var writer = file.writerStreaming(&.{}).interface;
+    const stdout = fs.File.stdout();
+    const stderr = fs.File.stderr();
 
-    const root = try cli.build(&writer, allocator);
+    var outWriter = stdout.writerStreaming(&.{});
+    var errWriter = stderr.writerStreaming(&.{});
+
+    logger.init(.debug, &errWriter.interface);
+    defer logger.deinit();
+
+    const root = try cli.build(&outWriter.interface, allocator);
     defer root.deinit();
 
     try root.execute(.{});
 
-    try writer.flush();
+    try outWriter.interface.flush();
+    try errWriter.interface.flush();
 }
